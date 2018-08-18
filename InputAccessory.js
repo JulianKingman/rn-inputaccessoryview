66f668bce1c1310a5ef57a396797d23fdb4b497f
Julian Kingman <julian@designman.io>
2018-08-17 23:18:45 -0700

    fix child render

--- InputAccessory.js
+++ InputAccessory.js
@@ -5,18 +5,29 @@ import withInputAccessory from './withInputAccessory';
 
 @withInputAccessory
 export default class InputAccessory extends Component {
+  static defaultProps = {
+    accessoryStyle: {},
+    wrapperStyle: {},
+  };
+
   constructor(props) {
     super(props);
   }
   render() {
-    const { id, accessoryStyle, wrapperStyle, children, functions } = this.props;
+    const {
+      id,
+      accessoryStyle,
+      wrapperStyle,
+      children,
+      functions,
+    } = this.props;
     return (
       <InputAccessoryView
         nativeID={id}
         style={[{ width: '100%', height: 40 }, accessoryStyle]}
       >
         <View style={[{ flexDirection: 'row', width: '100%' }, wrapperStyle]}>
-          {() => children(functions)}
+          {children(functions)}
         </View>
       </InputAccessoryView>
     );
