54f37709b8dbf7a8155d2a4b7256b1c539afff89
Julian Kingman <julian@designman.io>
2018-08-17 21:35:04 -0700

    index

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
-import React, { Component } from 'react';
-import hoistStatics from 'hoist-non-react-statics';
-
-const InputAccessoryContext = React.createContext({
-  setAccessoryFunction: null,
-});
-
-class InputAccessoryProvider extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-    this.setAccessoryFunction = this.setAccessoryFunction.bind(this);
-  }
-
-  setAccessoryFunction(key, fn) {
-    this.setState({ [key]: fn });
-  }
-
-  render() {
-    const { children } = this.props;
-    return (
-      <InputAccessoryContext.Provider
-        value={{
-          setAccessoryFunction: this.setAccessoryFunction,
-          ...this.state,
-        }}
-      >
-        {children}
-      </InputAccessoryContext.Provider>
-    );
-  }
-}
-
-const withInputAccessory = WrappedComponent =>
-  hoistStatics(
-    props => (
-      <InputAccessoryContext.Consumer>
-        {inputAccessoryProps => (
-          <WrappedComponent {...inputAccessoryProps} {...props} />
-        )}
-      </InputAccessoryContext.Consumer>
-    ),
-    WrappedComponent
-  );
+import withInputAccessory from './withInputAccessory';
+import InputAccessoryProvider from './InputAccessoryProvider';
+import InputAccessory from './InputAccessory';
 
 export default withInputAccessory;
-export { InputAccessoryProvider };
+export { InputAccessoryProvider, InputAccessory };
