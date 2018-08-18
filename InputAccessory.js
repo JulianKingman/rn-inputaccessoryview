46d7a5b071f21fc057313960ac03b21442b91589
Julian Kingman <julian@designman.io>
2018-08-17 23:36:26 -0700

    fix

--- InputAccessory.js
+++ InputAccessory.js
@@ -6,29 +6,23 @@ import withInputAccessory from './withInputAccessory';
 @withInputAccessory
 export default class InputAccessory extends Component {
   static defaultProps = {
-    accessoryStyle: {},
-    wrapperStyle: {},
+    style: {
+      backgroundColor: 'grey',
+    },
   };
 
   constructor(props) {
     super(props);
   }
   render() {
-    const {
-      id,
-      accessoryStyle,
-      wrapperStyle,
-      children,
-      functions,
-    } = this.props;
+    const { id, style, children, functions } = this.props;
     return (
       <InputAccessoryView
         nativeID={id}
-        style={[{ width: '100%', height: 40 }, accessoryStyle]}
+        style={[{ width: '100%', height: 40 }, style]}
+        backgroundColor={style.backgroundColor}
       >
-        <View style={[{ flexDirection: 'row', width: '100%' }, wrapperStyle]}>
-          {children(functions)}
-        </View>
+        {children(functions)}
       </InputAccessoryView>
     );
   }
